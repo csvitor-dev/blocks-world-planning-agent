c1e4f7438a3ca39ae60e21556e4646ecf8d780cd
csvitor-dev <csvitor.dev@gmail.com>
2025-11-28 13:14:07 -0300

    feat: improves domain class `StripsPlanning` according with notation

--- src/domain/strips_planning.py
+++ src/domain/strips_planning.py
@@ -1,5 +1,20 @@
 class StripsPlanning:
-    def __init__(self, actions: list[str], initial_state: str, final_state: str) -> None:
-        self.actions = actions
-        self.initial_state = initial_state
-        self.final_state = final_state
+    def __init__(self, actions: list[str], initial_state: str, goal_state: str) -> None:
+        self.__actions = self.__create_actions(actions)
+        self.__initial_state = initial_state
+        self.__goal_state = goal_state
+    
+    def __create_actions(self, raw_actions: list[str]) -> dict[str,tuple[str,str]]:
+        hook: dict[str,tuple[str,str]] = {}
+        for i in range(0, len(raw_actions), 3):
+            hook[raw_actions[i]] = (raw_actions[i+1], raw_actions[i+2])
+        return hook
+
+    def get_actions(self) -> dict[str,tuple[str,str]]:
+        return self.__actions
+
+    def get_states(self) -> dict[str, str]:
+        return {
+            'initial': self.__initial_state,
+            'goal': self.__goal_state
+        }
