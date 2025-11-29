43d06017532089cb1c089c38318d48015b1992ff
csvitor-dev <csvitor.dev@gmail.com>
2025-11-28 23:16:07 -0300

    feat: finish the expected structure of the `StripsPlanning` class

--- src/domain/strips_planning.py
+++ src/domain/strips_planning.py
@@ -1,20 +1,26 @@
 class StripsPlanning:
     def __init__(self, actions: list[str], initial_state: str, goal_state: str) -> None:
         self.__actions = self.__create_actions(actions)
-        self.__initial_state = initial_state
-        self.__goal_state = goal_state
+        self.__initial_state = self.__resolve_steps_group(initial_state)
+        self.__goal_state = self.__resolve_steps_group(goal_state)
     
-    def __create_actions(self, raw_actions: list[str]) -> dict[str,tuple[str,str]]:
-        hook: dict[str,tuple[str,str]] = {}
+    def __create_actions(self, raw_actions: list[str]) -> dict[str,dict[str,list[str]]]:
+        hook: dict[str,dict[str,list[str]]] = {}
         for i in range(0, len(raw_actions), 3):
-            hook[raw_actions[i]] = (raw_actions[i+1], raw_actions[i+2])
+            hook[raw_actions[i]] = {
+                'pre': self.__resolve_steps_group(raw_actions[i+1]),
+                'post': self.__resolve_steps_group(raw_actions[i+2])
+            }
         return hook
 
-    def get_actions(self) -> dict[str,tuple[str,str]]:
+    def __resolve_steps_group(self, raw_state: str) -> list[str]:
+        return raw_state.split(';')
+
+    def get_actions(self) -> dict[str,dict[str,list[str]]]:
         return self.__actions
 
-    def get_states(self) -> dict[str, str]:
+    def get_states(self) -> dict[str, list[str]]:
         return {
             'initial': self.__initial_state,
-            'goal': self.__goal_state
+            'goal': self.__goal_state,
         }
