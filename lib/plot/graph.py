a2a5ddf6028a54f957ad31a2ceb960e7369e28f6
csvitor-dev <csvitor.dev@gmail.com>
2025-12-01 21:53:30 -0300

    fix: correct implementation to explore graph

--- lib/plot/graph.py
+++ lib/plot/graph.py
@@ -10,40 +10,27 @@ def build_state_space(planning: Planning) -> nx.DiGraph[str]:
     G: nx.DiGraph[str] = nx.DiGraph()
     initial_state = planning.current_state
     queue = deque([initial_state])
-    visited = set([initial_state.identifier])
+    visited = set([initial_state.key])
 
     while queue:
         state: BlocksWorldState = queue.popleft()
-        G.add_node(state.identifier, label=str(state.current))
+        G.add_node(state.key, label=state.key)
 
         for next_state in state.successors(planning.actions):
-            next_id = next_state.identifier
+            G.add_node(next_state.key, label=next_state.key)
+            G.add_edge(state.key, next_state.key, label=next_state.identifier)
 
-            if G.has_edge(state.identifier, next_id) or G.has_edge(next_id, state.identifier):
-                continue
-            G.add_edge(state.identifier, next_id, label=next_id)
-
-            if next_id in visited:
-                continue
-            visited.add(next_id)
-            queue.append(next_state)
+            if next_state.key not in visited:
+                visited.add(next_state.key)
+                queue.append(next_state)
     return G
 
 
 def plot_graph(G: nx.DiGraph[str], generate_image: bool = False) -> None:
-    pos = nx.spring_layout(G, k=2, iterations=200, seed=42)
-
-    nx.draw_networkx_nodes(G, pos, node_color="lightblue", node_size=500)
-    nx.draw_networkx_edges(G, pos, arrows=True)
-
-    nx.draw_networkx_labels(G, pos, font_size=8)
-
-    edge_labels = {(u, v): data["label"] for u, v, data in G.edges(data=True)}
-    nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, font_size=7)
-    
     seed = random.randint(100, 100000)
     nx.nx_pydot.write_dot(G, f'./assets/output/graph-{seed}.dot')
 
-    if generate_image:
-        os.execv('/usr/bin/dot', ['dot', '-Tpng',
-                 f'./assets/output/graph-{seed}.dot', '-o', f'./assets/output/graph-{seed}.png'])
+    if generate_image is False:
+        return
+    os.execv('/usr/bin/dot', ['dot', '-Tpng',
+                              f'./assets/output/graph-{seed}.dot', '-o', f'./assets/output/graph-{seed}.png'])
