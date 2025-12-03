0845c74ed2ed42309007c2d5c68c3d6e18c7faaf
csvitor-dev <csvitor.dev@gmail.com>
2025-12-02 23:46:15 -0300

    refactor: improves `Planning` with context to execute or apply other algorithms

---
+++ src/support/factories/algorithm_factory.py
@@ -0,0 +1,21 @@
+from src.algorithms.a_star import AStar
+from src.algorithms.breadth_first_search import BFS
+from src.algorithms.depth_limitted_search import DLS
+from src.algorithms.iterative_deepening_search import IDS
+from src.domain.contracts.local_search_algorithm import LocalSearchAlgorithm
+from src.domain.contracts.planning_contract import PlanningContract
+
+
+class AlgorithmFactory:
+    __MAP: dict[str, type[LocalSearchAlgorithm]] = {
+        'A*': AStar,
+        'BFS': BFS,
+        'DLS': DLS,
+        'IDS': IDS,
+    }
+
+    @classmethod
+    def make(cls, key: str, planning: PlanningContract) -> LocalSearchAlgorithm:
+        if key not in cls.__MAP:
+            raise ValueError(f'Algorithm "{key}" is not supported')
+        return cls.__MAP[key](planning)
