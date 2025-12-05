72fc5a9c098f674b235d85835111ee0f2f9ff032
Erick Maia <erick.tech.oliveira@gmail.com>
2025-12-05 14:55:19 -0300

    feat: implements Iterative Deepening Search algorithm

--- src/algorithms/iterative_deepening_search.py
+++ src/algorithms/iterative_deepening_search.py
@@ -1,11 +1,21 @@
 from src.domain.contracts.local_search_algorithm import LocalSearchAlgorithm
 from src.domain.contracts.planning_contract import PlanningContract
+from src.algorithms.depth_limitted_search import DLS
 
 
 class IDS(LocalSearchAlgorithm):
     def __init__(self, planning: PlanningContract) -> None:
         super().__init__(planning)
+        self.__dls_instance = DLS(planning)
+        self.__num_generated = 0
 
-    def execute(self) -> None:
-        print("Executing IDS Algorithm")
-        print(self._planning.current_state)
+    def execute(self) -> tuple[list[str] | None, int, int]:
+        while True:
+            solution_path, N, explored_nodes = self.__dls_instance.execute()
+            self.__dls_instance.set_limit(1, to_increment=True)
+            self.__num_generated += N
+
+            if solution_path is not None:
+                return solution_path, self.__num_generated, explored_nodes
+        
+        
