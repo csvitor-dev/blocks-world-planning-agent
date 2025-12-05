6c9370ee66e94b6f3ea65d8aadd03b90d2cf5d8a
csvitor-dev <csvitor.dev@gmail.com>
2025-12-05 15:13:16 -0300

    refactor: improve class hierarchy to reusing instance variables

--- src/algorithms/iterative_deepening_search.py
+++ src/algorithms/iterative_deepening_search.py
@@ -7,15 +7,12 @@ class IDS(LocalSearchAlgorithm):
     def __init__(self, planning: PlanningContract) -> None:
         super().__init__(planning)
         self.__dls_instance = DLS(planning)
-        self.__num_generated = 0
 
     def execute(self) -> tuple[list[str] | None, int, int]:
         while True:
             solution_path, N, explored_nodes = self.__dls_instance.execute()
-            self.__dls_instance.set_limit(1, to_increment=True)
-            self.__num_generated += N
+            self.__dls_instance.increment_limit(1)
+            self._num_generated_nodes += N
 
             if solution_path is not None:
-                return solution_path, self.__num_generated, explored_nodes
-        
-        
+                return solution_path, self._num_generated_nodes, explored_nodes
--- src/domain/contracts/local_search_algorithm.py
+++ src/domain/contracts/local_search_algorithm.py
@@ -1,10 +1,21 @@
 from abc import ABC, abstractmethod
+from collections import deque
+from typing import Set
 from src.domain.contracts.planning_contract import PlanningContract
+from src.domain.blocks_world_state import BlocksWorldState
+
 
 class LocalSearchAlgorithm(ABC):
     def __init__(self, planning: PlanningContract) -> None:
         self._planning = planning
+        self._explored: Set[str] = set()
+        self._frontier: deque[BlocksWorldState] = deque()
+        self._goal: Set[int] = set(planning.states['goal'])
+        self._frontier.append(self._planning.current_state)
+        self._num_generated_nodes = 0
 
     @abstractmethod
-    def execute(self) -> tuple[list[str] | None, int, int]:
-        ...
+    def execute(self) -> tuple[list[str] | None, int, int]: ...
+
+    def is_goal_state(self, state: BlocksWorldState) -> bool:
+        return self._goal.issubset(state.current)
